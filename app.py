09e74716c98a4bae74a939a50a9716512e499538
unknown <samratkiev@gmail.com>
2023-09-11 16:58:15 +0200

    update and delete end points added

--- app.py
+++ app.py
@@ -12,25 +12,39 @@ app = Flask(__name__)
 data_manager = JSONDataManager(movie_data)
 
 
+def read_data():
+    with open("data/data.json", "r") as fileObject:
+        data=json.loads(fileObject.read())
+    return data
 
 @app.route('/')
 def home():
-    return "Welcome to MovieWeb App!"
+    """Function to implement home page of Movie web app"""
+    return render_template("home.html")
 
 @app.route("/users")
 def list_users():
+    """function to display all the users with their favourite movies."""
     users_list = data_manager.get_all_users()
-    return render_template("users.html", users=users_list)
+    user_id_list = data_manager.get_all_users_id()
+    return render_template("users.html", users=users_list, user_id_list =user_id_list)
 
 
 @app.route("/users/<int:user_id>")
 def user_movie_list(user_id):
+    """function to implement the add movie to a particular user with the given id."""
     user_movies = data_manager.get_user_movies(user_id)
-    return jsonify(user_movies)
+    user_list = data_manager.get_all_users()
+    user_id_list = data_manager.get_all_users_id()
+    index = user_id_list.index(user_id)
+    name = user_list[index]
+    return render_template("favourite_movie.html",user_name =name, movies = user_movies,user_id=user_id)
 
 @app.route("/add_user", methods = ["GET","POST"])
 def add_user():
+    """function to implement the add a user in the given user data."""
     if request.method == "POST":
+        users = read_data()
         name = request.form['name']
         if name == None:
             return render_template("users.html")
@@ -38,49 +52,103 @@ def add_user():
             users_id_list = data_manager.get_all_users_id()
             user = {}
             id = max(users_id_list) + 1
-            user[id]["name"] = name
+            user['name'] = name
+            users[str(id)] = user
+            user["movies"]=[]
             user_list = data_manager.get_all_users()
             user_list.append(name)
-            return redirect(url_for("index"))
+            with open("data/data.json", "w") as fileObject:
+                fileObject.write(json.dumps(users, indent=4))
+            return redirect(url_for("list_users"))
+    return render_template("add_user.html")
 
-    return render_template("index.html")
+@app.route("/delete_user/<int:user_id>", methods=["GET","DELETE"])
+def delete_user(user_id):
+    """function to implement the delete user from a given user list."""
+    all_user_data = read_data()
+    if str(user_id) in list(all_user_data.keys()):
+        all_user_data.pop(str(user_id))
+    with open("data/data.json", "w") as fileObject:
+        fileObject.write(json.dumps(all_user_data, indent=4))
+    return redirect(url_for("list_users"))
 
-
-
-@app.route("/users/<user_id>/add_movie", methods = ["POST"])
+@app.route("/users/<int:user_id>/add_movie", methods = ["GET","POST"])
 def add_user_movie(user_id):
+    """function to implement the add movie to particular user with a given user id"""
+    users_list = data_manager.get_all_users()
     users_id_list = data_manager.get_all_users_id()
-    user_list = data_manager.get_all_users()
+    index = users_id_list.index(user_id)
+    user_name = users_list[index]
+    users = read_data()
+    user_movie_list = data_manager.get_user_movies(user_id)
     user = {}
     movie = {}
-    name = request.form["name"]
+    if request.method == "POST":
+        name = request.form["name"]
+        Key = "2837c90f"
+        Url = f"http://www.omdbapi.com/?apikey={Key}&t={name}"
+        res = requests.get(Url)
+        data = res.json()
+        movie["movie_name"] = data["Title"]
+        movie["rating"] = data["imdbRating"]
+        movie["year"] = data["Year"]
+        movie["director"] = data["Director"]
+        movie["movie_id"] = len(user_movie_list) + 1
+        movie["poster"]= data["Poster"]
+        movie["note"]=[]
+        for user in users_list:
+            if user_id not in users_id_list:
+                return render_template("users.html")
+            elif user_id in users_id_list:
+                users[str(user_id)]["movies"].append(movie)
+                with open("data/data.json", "w") as fileObject:
+                    fileObject.write(json.dumps(users, indent=4))
+                return render_template("favourite_movie.html", user_name=user_name, movies=user_movie_list, user_id=user_id)
+    return render_template("add_movie.html", user_id=user_id)
 
-    Key = "2837c90f"
-    Url = f"http://www.omdbapi.com/?apikey={Key}&t={name}"
-    res = requests.get(Url)
-    data = res.json()
-    movie["name"] = data["Title"]
-    movie["rating"] = data["imdbRating"]
-    movie["year"] = data["Year"]
-    movie["director"] = data["Director"]
 
-    for user in user_list:
-        if user_id not in users_id_list:
-            return render_template("users.html")
-        elif user_id in users_id_list:
-            index = users_id_list.index(user_id)
-            user[user_id]["movies"].append(movie)
-            render_template("add_movie.html")
-
-@app.route("/users/<user_id>/update_movie/<movie_id>", methods = ["PUT"])
+@app.route("/users/<int:user_id>/update_movie/<int:movie_id>", methods=["GET", "POST"])
 def update_movie(user_id, movie_id):
-    pass
+    """function to implement the update the movie details with the given movie id for a
+        particular user with a given user id."""
+    users_list = data_manager.get_all_users()
+    users_id_list = data_manager.get_all_users_id()
+    index = users_id_list.index(user_id)
+    user_name = users_list[index]
+    users = read_data()
+    user_movie_list = data_manager.get_user_movies(user_id)
+    if request.method == "post":
+        rating = request.form["rating"]
+        note = request.form["note"]
+        for movie in user_movie_list:
+            if int(movie["movie_id"]) == movie_id:
+                movie.rating = rating
+                movie.note = note
+                with open("data/data.json", "w") as fileObject:
+                    fileObject.write(json.dumps(users, indent=4))
+                return render_template("favourite_movie.html", user_name=user_name, user_id=user_id,movie_id=movie["movie_id"],movies=user_movie_list)
 
-@app.route("/users/<user_id>/delete_movie/<movie_id>", methods = ["DELETE"])
+    return render_template("update.html", user_name=user_name, movies=user_movie_list, user_id=user_id)
+@app.route("/users/<int:user_id>/delete_movie/<int:movie_id>")
 def delete_movie(user_id, movie_id):
-    pass
+    """function to implement to delete a movie with a given id for a given user"""
+    users_list = data_manager.get_all_users()
+    users_id_list = data_manager.get_all_users_id()
+    index = users_id_list.index(user_id)
+    user_name = users_list[index]
+    users = read_data()
+    user_movie_list = data_manager.get_user_movies(user_id)
+    for movie in user_movie_list:
+        if int(movie["movie_id"]) == movie_id:
+            users[str(user_id)]["movies"].remove(movie)
+    with open("data/data.json", "w") as fileObject:
+        fileObject.write(json.dumps(users, indent=4))
+    return render_template("favourite_movie.html", user_name=user_name, movies=user_movie_list, user_id=user_id)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- data_managers/data_manager_interface_json.py
+++ data_managers/data_manager_interface_json.py
@@ -1,7 +1,11 @@
 import json
 import self as self
 from .data_manager_interface import DataManagerInterface
+import json
+import os
 
+# Define the path to the data.json file
+data_file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'data.json')
 
 
 def file_data(filename):
@@ -17,13 +21,22 @@ class JSONDataManager(DataManagerInterface):
     def get_all_users(self):
         # Return all the users all users
         data = file_data(self.filename)
-        users_list = [ value["name"] for key, value in data.items()]
+        users_list = [value['name'] for key, value in data.items()]
         return users_list
 
     def get_all_users_id(self):
         data = file_data(self.filename)
         users_id_list = [int(key) for key, value in data.items()]
         return users_id_list
+
+    def get_all_movies_id(self, user_id):
+        data = file_data(self.filename)
+        user_movie_id_list = []
+        for key, value in data.items():
+            if int(key) == user_id:
+                user_movie_id_list.append(value["movies"]["movie_id"])
+                continue
+        return user_movie_id_list
 
 
     def get_user_movies(self, user_id):
@@ -38,3 +51,6 @@ class JSONDataManager(DataManagerInterface):
 
 
 
+obj = JSONDataManager(data_file_path)
+x=obj.get_all_movies_id("")
+print(x)
